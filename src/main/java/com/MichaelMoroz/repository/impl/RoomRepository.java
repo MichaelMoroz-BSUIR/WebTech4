a08f0ab4a854508cfa3ca2d275a129dc7d2e2d01
MichaelMoroz-BSUIR <mishamoroz50@gmail.com>
2021-12-11 20:10:34 +0300

    Initial commit

---
+++ src/main/java/com/MichaelMoroz/repository/impl/RoomRepository.java
@@ -0,0 +1,56 @@
+package com.MichaelMoroz.repository.impl;
+
+import com.MichaelMoroz.builder.RoomBuilder;
+import com.MichaelMoroz.entity.Room;
+import com.MichaelMoroz.exception.RepositoryException;
+import com.MichaelMoroz.repository.AbstractRepository;
+import com.MichaelMoroz.specification.Specification;
+
+import java.sql.Connection;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class RoomRepository extends AbstractRepository<Room> {
+    private static final String TABLE_NAME = " `room` ";
+
+    private static final String ID = "id";
+    private static final String ROOM_NUMBER = "room_number";
+    private static final String OCCUPIED = "occupied";
+
+    private static final String SELECT_QUERY = "SELECT * FROM `room` ";
+
+    public RoomRepository(Connection connection) {
+        super(connection);
+    }
+
+    @Override
+    public Map<String, Object> getFields(Room item) {
+        Map<String, Object> values = new LinkedHashMap<>();
+        values.put(ROOM_NUMBER, item.getRoomNumber());
+        values.put(OCCUPIED, item.getOccupied());
+        values.put(ID, item.getId());
+
+        return values;
+    }
+
+    @Override
+    public String getTableName() {
+        return TABLE_NAME;
+    }
+
+    @Override
+    public Optional<Room> query(Specification specification) throws RepositoryException {
+        String query = SELECT_QUERY + specification.toSql();
+        List<Object> params = specification.getParameters();
+        return executeQueryForSingleResult(query, new RoomBuilder(), params);
+    }
+
+    @Override
+    public List<Room> queryAll(Specification specification) throws RepositoryException {
+        String query = SELECT_QUERY + specification.toSql();
+        List<Object> params = specification.getParameters();
+        return executeQuery(query, new RoomBuilder(), params);
+    }
+}
---
+++ src/main/java/com/MichaelMoroz/specification/room/FindFree.java
@@ -0,0 +1,22 @@
+package com.MichaelMoroz.specification.room;
+
+import com.MichaelMoroz.specification.Specification;
+
+import java.util.Collections;
+import java.util.List;
+
+public class FindFree implements Specification {
+
+    public FindFree() {
+    }
+
+    @Override
+    public String toSql() {
+        return "WHERE occupied = 'false'";
+    }
+
+    @Override
+    public List<Object> getParameters() {
+        return Collections.emptyList();
+    }
+}
